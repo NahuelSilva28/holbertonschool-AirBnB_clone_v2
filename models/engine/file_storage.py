a9c311a4aaeb2a94e9a429f7270f8aa9d7768775
Gabr1el20 <6327@holbertonstudents.com>
2023-07-31 12:39:36 -0700

    Task 5 - all method  -

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            dict_filtered = {}
+            for k, v in self.__objects.items():
+                if isinstance(v, cls):
+                    dict_filtered[k] = v
+            return dict_filtered
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
