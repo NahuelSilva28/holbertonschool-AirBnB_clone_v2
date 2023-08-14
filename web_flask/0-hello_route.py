127f1674fda3dbe70cf2ce861a2fb865939ad123
Gabr1el20 <6327@holbertonstudents.com>
2023-08-14 06:34:06 -0700

    Added read me

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"Starts a Web Application"
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -12,4 +12,5 @@ def hbnb():
 
 
 if __name__ == "__main__":
+    """Starts a web app"""
     app.run(debug=True, host="0.0.0.0", port=5000)
