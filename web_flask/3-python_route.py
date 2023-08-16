2480460f44fda86bb196fad6c407c8611af76a45
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-16 16:51:49 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Flask Web Application"""
 
+
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def hello_hbnb():
