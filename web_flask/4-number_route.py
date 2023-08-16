555b9d55ef8bcf54744490ce5446fd35778da3eb
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-16 17:36:52 +0000

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Flask Web Application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    """Route for the homepage"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", methods=['GET'], strict_slashes=False)
+def hbnb():
+    """Route for /hbnb"""
+    return "HBNB"
+
+@app.route("/c/<text>", methods=['GET'], strict_slashes=False)
+def c_text(text):
+    """Route for /c/<text>"""
+    return "C {}".format(text.replace("_", " "))
+
+@app.route("/python/", defaults={"text": "is cool"}, methods=['GET'], strict_slashes=False)
+@app.route("/python/<text>", methods=['GET'], strict_slashes=False)
+def python_text(text):
+    """Route for /python/<text> and /python"""
+    return "Python {}".format(text.replace("_", " "))
+
+@app.route("/number/<int:n>", methods=['GET'], strict_slashes=False)
+def number_n(n):
+    """Route for /number/<n>"""
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
